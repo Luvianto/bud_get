5c62fbfd121c2fe565a88fd16ff7ab49c36b4ab5
Luvianto <126235306+Luvianto@users.noreply.github.com>
2025-10-30 22:35:54 +0700

    Refactor interface class into datasource

--- lib/module/outcome/datasource/outcome_datasource.dart
+++ lib/module/outcome/datasource/outcome_datasource.dart
@@ -1,7 +1,7 @@
 import 'package:bud_get/chore/handler/either.dart';
 import 'package:bud_get/module/outcome/model/outcome_model.dart';
 
-abstract class OutcomeInterface {
+abstract class OutcomeDatasource {
   Future<Either<Exception, List<OutcomeModel>>> getOutcomes();
   Future<Either<Exception, OutcomeModel>> createOutcome(
     OutcomeModel outcomeModel,
